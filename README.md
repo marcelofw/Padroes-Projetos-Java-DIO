d6247b452992b8777b4a409647f48363d1e5546f
marcelo_fw <marcelo_fw@outlook.com>
2022-08-02 21:30:30 -0300

    Project delivery

---
+++ README.md
@@ -0,0 +1,10 @@
+# Explorando Padrões de Projetos na Prática com Java
+
+
+Este projeto faz parte do treinamento do curso da Digital Innovation, para o bootcamp Spring Framework Experience.
+
+
+Repositório com as implementações dos padrões de projetos explorados no Lab "Explorando Padrões de Projetos na Prática com Java". 
+- Singleton
+- Strategy/Repository
+- Facade
---
+++ src/main/java/br/com/dio/desafio/Application.java
@@ -0,0 +1,19 @@
+package br.com.dio.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ *
+ * @author carmo.junior
+ */
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
